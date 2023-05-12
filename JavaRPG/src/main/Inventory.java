5cd5b5c5c0a560aa752cf31a32ed27248558bf5c
matheus <matheusmw21@gmail.com>
2023-05-11 22:25:36 -0300

    fix: chooseItem method

--- JavaRPG/src/main/Inventory.java
+++ JavaRPG/src/main/Inventory.java
@@ -24,24 +24,34 @@ public class Inventory {
 
     public static String chooseItem() {
         String itemMsg = "";
-        if (Window.getUserInput().equals("1")) {
+        String userInput = Window.getUserInput();
+        int index;
+        try {
+            index = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            return "Opção inválida!\n";
+        }
+        if (index < 1 || index > map.size()) {
+            return "Opção inválida!\n";
+        }
+        if (index == 1) {
             // defesa
             double defensePoints = GameLogic.player.increaseDefense();
             System.out.println(defensePoints);
             itemMsg += "Você usou o escudo e ganhou " + defensePoints + " pontos de defesa!\n";
-            removeItem(1);
-        } else if (Window.getUserInput().equals("2")) {
+            // removeItem(1);
+        } else if (index == 2) {
             // aumenta força
             double damagePoints = GameLogic.player.increaseStrength();
             System.out.println(damagePoints);
             itemMsg += "Você usou durateston e ganhou " + damagePoints + " pontos de ataque!\n";
-            removeItem(2);
-        } else if (Window.getUserInput().equals("3")) {
+            // removeItem(2);
+        } else if (index == 3) {
             // uso do estimulante
             double hpPoints = GameLogic.player.increaseHp();
             System.out.println(hpPoints);
             itemMsg += "Você usou o estimulante e ganhou " + hpPoints + " de vida!\n";
-            removeItem(3);
+            // removeItem(3);
         }
         return itemMsg;
     }
@@ -66,15 +76,14 @@ public class Inventory {
     }
 
     public static void removeItem(int index) {
-        // checking if the index is within the bounds of the array
-        /*
-         * if (items.cotainsKey(index)) {
-         * String item = items.get(index);
-         * GameLogic.player.items.remove(item);
-         * items.remove(index);
-         * }
-         */
+        // checking if the index is within the bounds of the map
+        if (map.containsKey(index)) {
+            // remove the item from the player's items list
+            String itemName = map.remove(index);
+            GameLogic.player.items.remove(itemName);
 
+            Window.setDisplayText(itemName + " foi removido do seu inventário!");
+        }
     }
 
 }
